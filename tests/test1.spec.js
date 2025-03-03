bce58708fbe464374b3a9da142768dcf4942a30d
Sebastian Gerritsen <136114185+sgerritsen2@users.noreply.github.com>
2025-03-03 08:41:34 -0600

    Update test1.spec.js

--- tests/test1.spec.js
+++ tests/test1.spec.js
@@ -1,4 +1,4 @@
- const { Builder, By, Key, until } = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 const assert = require('assert');
 const chrome = require('selenium-webdriver/chrome');
 
